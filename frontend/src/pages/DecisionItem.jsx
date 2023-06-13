2f12ec2e1f0774994d82e37b5af4fd86085fce5e
BenoitLafabrie <[benoit_lafabrie_pro@fastmail.com](mailto:benoit_lafabrie_pro@fastmail.com)>
2023-06-13 17:12:35 +0200

    Content loader functionnal

--- frontend/src/pages/DecisionItem.jsx
+++ frontend/src/pages/DecisionItem.jsx
@@ -3,52 +3,56 @@ import * as React from "react";
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
-import { Chip } from "@mui/material";
+import { Box, Chip } from "@mui/material";
 import Typography from "@mui/material/Typography";
 import Avatar from "@mui/material/Avatar";
 import Stack from "@mui/material/Stack";
 import { deepPurple } from "@mui/material/colors";
+import DecisionLoader from "../components/DecisionLoader";
 
 export default function DecisionItem() {
   return (
-    <Card sx={{ maxWidth: 280 }}>
-      <Chip
-        variant="outlined"
-        color="success"
-        size="small"
-        label="Décision en cours"
-        sx={{ marginLeft: "5%", marginTop: "1%" }}
-      />
-      <Chip
-        variant="outlined"
-        color="error"
-        size="small"
-        label="Hub France"
-        sx={{ marginLeft: "1%", marginTop: "1%" }}
-      />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          Décision factice
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          Texte factice d'une decision factice dans ma vie factice...
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Stack direction="row" spacing={2}>
-          <Avatar sx={{ bgcolor: deepPurple[500], marginLeft: "1%" }}>
-            JD
-          </Avatar>
-        </Stack>
-        <Typography
-          variant="caption"
-          display="block"
-          gutterBottom
-          sx={{ marginLeft: "5%" }}
-        >
-          par Jane Doe
-        </Typography>
-      </CardActions>
-    </Card>
+    <Box>
+      <DecisionLoader />
+      <Card sx={{ maxWidth: 280 }}>
+        <Chip
+          variant="outlined"
+          color="success"
+          size="small"
+          label="Décision en cours"
+          sx={{ marginLeft: "5%", marginTop: "1%" }}
+        />
+        <Chip
+          variant="outlined"
+          color="error"
+          size="small"
+          label="Hub France"
+          sx={{ marginLeft: "1%", marginTop: "1%" }}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Décision factice
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Texte factice d'une decision factice dans ma vie factice...
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Stack direction="row" spacing={2}>
+            <Avatar sx={{ bgcolor: deepPurple[500], marginLeft: "1%" }}>
+              JD
+            </Avatar>
+          </Stack>
+          <Typography
+            variant="caption"
+            display="block"
+            gutterBottom
+            sx={{ marginLeft: "5%" }}
+          >
+            par Jane Doe
+          </Typography>
+        </CardActions>
+      </Card>
+    </Box>
   );
 }
