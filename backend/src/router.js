9e21173292c5e818186e5152fffc20edc9ef3fad
BenoitLafabrie <[benoit_lafabrie_pro@fastmail.com](mailto:benoit_lafabrie_pro@fastmail.com)>
2023-06-13 12:12:34 +0200

    CRUD survey done

--- backend/src/router.js
+++ backend/src/router.js
@@ -4,6 +4,7 @@ const router = express.Router();
 
 const decisionControllers = require("./controllers/decisionControllers");
 const userControllers = require("./controllers/userControllers");
+const surveyControllers = require("./controllers/surveyControllers");
 
 router.get("/decisions", decisionControllers.getAllDecisions);
 router.get("/decisions/:id", decisionControllers.getOneDecision);
@@ -16,5 +17,11 @@ router.get("/users/:id", userControllers.getOneUser);
 router.put("/users/:id", userControllers.updateUser);
 router.post("/users", userControllers.addUser);
 router.delete("/users/:id", userControllers.deleteUser);
+
+router.get("/surveys", surveyControllers.getAllSurveys);
+router.get("/surveys/:id", surveyControllers.getOneSurvey);
+router.put("/surveys/:id", surveyControllers.updateSurvey);
+router.post("/surveys", surveyControllers.addSurvey);
+router.delete("/surveys/:id", surveyControllers.deleteSurvey);
 
 module.exports = router;
