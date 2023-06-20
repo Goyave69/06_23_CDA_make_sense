ef1fe61957f37a2182443e2c4dae38e6b4761467
BenoitLafabrie <[benoit_lafabrie_pro@fastmail.com](mailto:benoit_lafabrie_pro@fastmail.com)>
2023-06-20 09:43:10 +0200

    Login working

--- backend/src/router.js
+++ backend/src/router.js
@@ -2,41 +2,53 @@ const express = require("express");
 
 const router = express.Router();
 
+// Controllers
 const decisionControllers = require("./controllers/decisionControllers");
 const userControllers = require("./controllers/userControllers");
 const surveyControllers = require("./controllers/surveyControllers");
+const loginController = require("./controllers/authControllers");
+
+// Validators
 const {
   validateDecision,
   validateUser,
   validateSurvey,
 } = require("./services/validators");
+
+// Hashing
 const { hashPassword } = require("./services/passwordHelper");
 
-router.get("/decisions", decisionControllers.getAllDecisions);
-router.get("/decisions/:id", decisionControllers.getOneDecision);
-router.put(
-  "/decisions/:id",
-  validateDecision,
-  decisionControllers.updateDecision
-);
+// POST Routes
+router.post("/login", loginController.login);
+router.post("/users", validateUser, hashPassword, userControllers.addUser);
 router.post("/decisions", validateDecision, decisionControllers.addDecision);
-router.delete("/decisions/:id", decisionControllers.deleteDecision);
+router.post("/surveys", validateSurvey, surveyControllers.addSurvey);
 
+// GET Routes
 router.get("/users", userControllers.getAllUsers);
 router.get("/users/:id", userControllers.getOneUser);
+router.get("/decisions", decisionControllers.getAllDecisions);
+router.get("/decisions/:id", decisionControllers.getOneDecision);
+router.get("/surveys", surveyControllers.getAllSurveys);
+router.get("/surveys/:id", surveyControllers.getOneSurvey);
+
+// PUT Routes
 router.put(
   "/users/:id",
   validateUser,
   hashPassword,
   userControllers.updateUser
 );
-router.post("/users", validateUser, hashPassword, userControllers.addUser);
-router.delete("/users/:id", userControllers.deleteUser);
-
-router.get("/surveys", surveyControllers.getAllSurveys);
-router.get("/surveys/:id", surveyControllers.getOneSurvey);
+router.put(
+  "/decisions/:id",
+  validateDecision,
+  decisionControllers.updateDecision
+);
 router.put("/surveys/:id", validateSurvey, surveyControllers.updateSurvey);
-router.post("/surveys", validateSurvey, surveyControllers.addSurvey);
+
+// DELETE Routes
+router.delete("/users/:id", userControllers.deleteUser);
+router.delete("/decisions/:id", decisionControllers.deleteDecision);
 router.delete("/surveys/:id", surveyControllers.deleteSurvey);
 
 module.exports = router;
--- backend/src/services/passwordHelper.js
+++ backend/src/services/passwordHelper.js
@@ -24,25 +24,12 @@ const hashPassword = (req, res, next) => {
     });
 };
 
-const verifyPassword = (req, res) => {
-  argon2
-    .verify(req.user.hashedPassword, req.body.password)
-    .then((isVerified) => {
-      if (isVerified) {
-        const payload = { sub: req.user.id };
-        const token = jwt.sign(payload, process.env.JWT_SECRET, {
-          expiresIn: "1h",
-        });
-        delete req.user.hashedPassword;
-        res.send({ token, user: req.user });
-      } else {
-        res.sendStatus(401);
-      }
-    })
-    .catch((err) => {
-      console.error(err);
-      res.sendStatus(500);
-    });
+const verifyPassword = async (hashedPassword, plainPassword) => {
+  try {
+    return await argon2.verify(hashedPassword, plainPassword);
+  } catch (err) {
+    return console.error(err);
+  }
 };
 
 const verifyToken = (req, res, next) => {
