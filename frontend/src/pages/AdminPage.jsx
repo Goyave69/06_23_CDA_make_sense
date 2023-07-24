47a5b8be19ee792da50e181382cf1713fd7f1631
BenoitLafabrie <[benoit_lafabrie_pro@fastmail.com](mailto:benoit_lafabrie_pro@fastmail.com)>
2023-07-24 15:38:50 +0200

    Alert for both buttons functionnal

--- frontend/src/pages/AdminPage.jsx
+++ frontend/src/pages/AdminPage.jsx
@@ -1,5 +1,12 @@
-import { React, useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import {
+  AlertDialog,
+  AlertDialogBody,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogContent,
+  AlertDialogOverlay,
+  AlertDialogCloseButton,
   Button,
   ButtonGroup,
   Grid,
@@ -12,6 +19,7 @@ import {
   Th,
   Td,
   TableContainer,
+  useDisclosure,
 } from "@chakra-ui/react";
 import { DeleteIcon, EditIcon } from "@chakra-ui/icons";
 import loadData from "../services/loadData";
@@ -19,6 +27,17 @@ import loadData from "../services/loadData";
 export default function AdminPage() {
   const [userData, setUserData] = useState([]);
   const allUsers = [userData];
+  const {
+    isOpen: isEditAlertOpen,
+    onOpen: onEditAlertOpen,
+    onClose: onEditAlertClose,
+  } = useDisclosure();
+  const {
+    isOpen: isDeleteAlertOpen,
+    onOpen: onDeleteAlertOpen,
+    onClose: onDeleteAlertClose,
+  } = useDisclosure();
+  const cancelRef = useRef();
 
   useEffect(() => {
     loadData("users", setUserData);
@@ -33,13 +52,18 @@ export default function AdminPage() {
       </GridItem>
       <GridItem margin="0 5% 5% 5%">
         <TableContainer border="1px" borderColor="#0C3944" borderRadius="10">
-          <Table variant="striped" colorScheme="whatsapp" marginTop="0.5%">
+          <Table
+            variant="striped"
+            colorScheme="whatsapp"
+            marginTop="0.5%"
+            size="sm"
+          >
             <Thead>
               <Tr>
                 <Th>Name</Th>
                 <Th>Birthdate</Th>
                 <Th>Email</Th>
-                <Th>Actions</Th>
+                <Th marginRight="-5%">Actions</Th>
               </Tr>
             </Thead>
             <Tbody>
@@ -58,8 +82,93 @@ export default function AdminPage() {
                         variant="outlined"
                         marginLeft="-5%"
                       >
-                        <Button leftIcon={<EditIcon />}>Edit</Button>
-                        <Button leftIcon={<DeleteIcon />}>Delete</Button>
+                        <>
+                          <Button
+                            leftIcon={<EditIcon />}
+                            onClick={onEditAlertOpen}
+                          >
+                            Edit
+                          </Button>
+                          <AlertDialog
+                            motionPreset="slideInBottom"
+                            leastDestructiveRef={cancelRef}
+                            onClose={onEditAlertClose}
+                            isOpen={isEditAlertOpen}
+                            isCentered
+                          >
+                            <AlertDialogOverlay bg="blackAlpha.200" />
+
+                            <AlertDialogContent bg="#9B084F" color="#FCF6A0">
+                              <AlertDialogHeader>
+                                Confirm changes?
+                              </AlertDialogHeader>
+                              <AlertDialogCloseButton />
+                              <AlertDialogBody>
+                                Are you sure you want to edit this user?
+                              </AlertDialogBody>
+                              <AlertDialogFooter>
+                                <Button
+                                  ref={cancelRef}
+                                  onClick={onEditAlertClose}
+                                  _hover={{ bg: "#E36164" }}
+                                >
+                                  No
+                                </Button>
+                                <Button
+                                  colorScheme="red"
+                                  ml={3}
+                                  _hover={{ bg: "#E36164" }}
+                                >
+                                  Yes
+                                </Button>
+                              </AlertDialogFooter>
+                            </AlertDialogContent>
+                          </AlertDialog>
+                        </>
+                        <>
+                          <Button
+                            leftIcon={<DeleteIcon />}
+                            onClick={onDeleteAlertOpen}
+                          >
+                            Delete
+                          </Button>
+                          <AlertDialog
+                            motionPreset="slideInBottom"
+                            leastDestructiveRef={cancelRef}
+                            onClose={onDeleteAlertClose}
+                            isOpen={isDeleteAlertOpen}
+                            isCentered
+                          >
+                            <AlertDialogOverlay bg="blackAlpha.200" />
+
+                            <AlertDialogContent bg="#9B084F" color="#FCF6A0">
+                              <AlertDialogHeader>
+                                Confirm Deletion?
+                              </AlertDialogHeader>
+                              <AlertDialogCloseButton />
+                              <AlertDialogBody>
+                                Are you sure? You can't undo this action
+                                afterwards.
+                              </AlertDialogBody>
+                              <AlertDialogFooter>
+                                <Button
+                                  ref={cancelRef}
+                                  onClick={onDeleteAlertClose}
+                                  _hover={{ bg: "#E36164" }}
+                                >
+                                  No
+                                </Button>
+                                <Button
+                                  colorScheme="red"
+                                  ml={3}
+                                  _hover={{ bg: "#E36164" }}
+                                >
+                                  Yes
+                                </Button>
+                              </AlertDialogFooter>
+                            </AlertDialogContent>
+                          </AlertDialog>
+                        </>
                       </ButtonGroup>
                     </Td>
                   </Tr>
